8ef84a2430703f1239c1404ac26dd6288fcedaed
DeusExDev <enriquemartin1402@gmail.com>
2025-03-05 18:06:27 +0000

    Change codesnap config

--- nvim/lua/plugins/codesnap.lua
+++ nvim/lua/plugins/codesnap.lua
@@ -19,10 +19,11 @@ return {
     save_path = "~/Downloads/",
     has_breadcrumbs = false,
     has_line_number = true,
-    bg_theme = "dusk",
+    bg_theme = "grape",
     watermark = "tyrell_w3llick",
     code_font_family = "JetBrainsMono Nerd Font",
     watermark_font_family = "Roboto",
     min_width = 0,
+    mac_window_scale_factor = 1.15,
   },
 }
