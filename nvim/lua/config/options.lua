a0add4d7382cf45e3c96cfe0cadf643fc9bfb713
DeusExDev <enriquemartin1402@gmail.com>
2025-03-18 21:28:38 +0000

    change nvim tabs

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,3 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.guicursor = "i:block-blinkwait0-blinkon100-blinkoff100"
+vim.opt.expandtab = false
