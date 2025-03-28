346e29ad16d00d29dd999516e92d3b54eb632274
DeusExDev <enriquemartin1402@gmail.com>
2025-03-28 21:52:36 +0000

    add biome support for nvim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,4 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.guicursor = "i:block-blinkwait0-blinkon100-blinkoff100"
-vim.opt.expandtab = false
+
+-- Avoid conflicts between biome and prettier
+vim.g.lazyvim_prettier_needs_config = true
