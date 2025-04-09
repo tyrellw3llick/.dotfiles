a2a3c8fc93c22ebabc79d008b512e16ba8ae8f04
DeusExDev <enriquemartin1402@gmail.com>
2025-04-09 23:07:17 +0100

    new nvim config from scratch
    finally

--- nvim/lua/config/options.lua
+++
@@ -1,7 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-vim.opt.guicursor = "i:block-blinkwait0-blinkon100-blinkoff100"
-
--- Avoid conflicts between biome and prettier
-vim.g.lazyvim_prettier_needs_config = true
