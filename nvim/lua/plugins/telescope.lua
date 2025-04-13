f6cd91d40fe163fda49aa607e409c570f6c13303
DeusExDev <enriquemartin1402@gmail.com>
2025-04-13 05:33:33 +0100

    Add telescope undo

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,14 +5,16 @@ return {
 		"nvim-lua/plenary.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		"nvim-tree/nvim-web-devicons",
+		"debugloop/telescope-undo.nvim",
 	},
 	config = function()
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
+		local telescope_undo = require("telescope-undo")
 
 		telescope.setup({
 			defaults = {
-				path_display = {"smart"},
+				path_display = { "smart" },
 				mappings = {
 					i = {
 						["<C-k>"] = actions.move_selection_previous,
@@ -21,9 +23,16 @@ return {
 					},
 				},
 			},
+			extensions = {
+				undo = {
+					telescope_undo,
+				},
+			},
 		})
 
-		telescope.load_extension("fzf")
+		vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>", { desc = "Open undo-tree" })
 
+		telescope.load_extension("fzf")
+		telescope.load_extension("undo")
 	end,
 }
