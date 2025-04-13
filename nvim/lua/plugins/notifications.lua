da96086f68ca9bbbe5ecdfbc930bfa024aa47fbb
DeusExDev <enriquemartin1402@gmail.com>
2025-04-14 00:13:59 +0100

    change notification settings

--- nvim/lua/plugins/notifications.lua
+++ nvim/lua/plugins/notifications.lua
@@ -31,6 +31,19 @@ return {
 				inc_rename = false, -- enables an input dialog for inc-rename.nvim
 				lsp_doc_border = true, -- add a border to hover docs and signature help
 			},
+			routes = {
+				{
+					filter = {
+						event = "msg_show",
+						any = {
+							{ find = "%d+L, %d+B" },
+							{ find = "; after #%d+" },
+							{ find = "; before #%d+" },
+						},
+					},
+					view = "notify",
+				},
+			},
 		})
 	end,
 }
