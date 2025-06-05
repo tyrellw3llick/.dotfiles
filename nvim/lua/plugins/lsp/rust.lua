397f8b9159f0b3d0e0120c3d95db2b959489178a
TyrellW3llick <enriquemartin1402@gmail.com>
2025-06-05 23:30:18 +0200

    add support for rust

--- nvim/lua/plugins/lsp/rust.lua
+++ nvim/lua/plugins/lsp/rust.lua
@@ -1,6 +1,60 @@
--- TODO: add some keymaps for useful commands when I learn the language better
 return {
 	"mrcjkb/rustaceanvim",
-	version = "^6", -- Recommended
+	version = "^6",
 	lazy = false,
+	opts = {
+		server = {
+			on_attach = function(client, bufnr)
+				vim.keymap.set("n", "<leader>cr", function()
+					vim.cmd.RustLsp("codeAction")
+				end, { desc = "Code Action", buffer = bufnr })
+				vim.keymap.set("n", "<leader>dr", function()
+					vim.cmd.RustLsp("debuggables")
+				end, { desc = "Rust Debuggables", buffer = bufnr })
+				vim.keymap.set("n", "K", function()
+					vim.cmd.RustLsp({ "hover", "actions" })
+				end, { silent = true, desc = "Hover actions", buffer = bufnr })
+			end,
+			default_settings = {
+				["rust-analyzer"] = {
+					cargo = {
+						allFeatures = true,
+						loadOutDirsFromCheck = true,
+						buildScripts = {
+							enable = true,
+						},
+					},
+					checkOnSave = true,
+					diagnostics = {
+						enable = true,
+					},
+					procMacro = {
+						enable = true,
+						ignored = {
+							["async-trait"] = { "async_trait" },
+							["napi-derive"] = { "napi" },
+							["async-recursion"] = { "async_recursion" },
+						},
+					},
+					files = {
+						excludeDirs = {
+							".direnv",
+							".git",
+							".github",
+							".gitlab",
+							"bin",
+							"node_modules",
+							"target",
+							"venv",
+							".venv",
+						},
+					},
+				},
+			},
+		},
+	},
+	config = function(_, opts)
+		vim.g.rustaceanvim_meta = vim.g.rustaceanvim_meta or {}
+		vim.tbl_deep_extend("force", vim.g.rustaceanvim_meta, opts)
+	end,
 }
